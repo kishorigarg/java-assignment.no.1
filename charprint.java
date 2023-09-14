c4f5afceb561e2258eea2ae3a52db3e18dbe44b3
kishorigarg <143869466+kishorigarg@users.noreply.github.com>
2023-09-14 19:06:41 +0530

    Add files via upload

---
+++ charprint.java
@@ -0,0 +1,10 @@
+#8
+import java.util.*;
+public class Prog10{
+    public static void main(String[] args) {
+        for(char i='A';i<='Z';i++){
+            System.out.print(i+"    ");
+        }
+    }
+
+}
