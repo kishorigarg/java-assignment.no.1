900874e25b90761d4106e4cf7d48bf8e5cb24d80
kishorigarg <143869466+kishorigarg@users.noreply.github.com>
2023-09-14 19:10:39 +0530

    Add files via upload

---
+++ char.java
@@ -0,0 +1,10 @@
+#7
+import java.util.*;
+class Prog1{
+    public static void main(String[] args){
+        System.out.print("Enter any number:   ");
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.println("The number entered by the user is "+n);
+    }
+}
---
+++ datatype..2.java
@@ -0,0 +1,35 @@
+#6public class Prog2{
+    public static void main(String[] args) {
+        // boolean
+        boolean isJavaFun = true;
+        System.out.println("Is Java fun? " + isJavaFun);
+
+        // char
+        char grade = 'A';
+        System.out.println("Your grade is: " + grade);
+
+        // byte
+        byte smallNumber = 10;
+        System.out.println("A small number: " + smallNumber);
+
+        // short
+        short smallInteger = 1000;
+        System.out.println("A small integer: " + smallInteger);
+
+        // int
+        int age = 25;
+        System.out.println("Your age is: " + age);
+
+        // long
+        long bigNumber = 1234567890L; // Note the 'L' suffix for long literals
+        System.out.println("A big number: " + bigNumber);
+
+        // float
+        float piApproximation = 3.14159f; // Note the 'f' suffix for float literals
+        System.out.println("Approximate value of pi: " + piApproximation);
+
+        // double
+        double pi = 3.141592653589793;
+        System.out.println("Accurate value of pi: " + pi);
+    }
+}
