07028dd3a57075ea0ee80a6fdda3e25f99867938
kishorigarg <143869466+kishorigarg@users.noreply.github.com>
2023-09-14 20:17:37 +0530

    Add files via upload

---
+++ joint.java
@@ -0,0 +1,14 @@
+//join two strings
+class joint{
+    public static void main(String[]args){
+        System.out.println("\nkishori garg");
+        String first="java";
+        System.out.println("first string"+first);
+        String second="Programming";
+        System.out.println("second string:"+second);
+        StringjoinedString=first.concat(second);
+        System.out.println("JoinedString:"+JoinedString);
+        
+        
+    }
+}
