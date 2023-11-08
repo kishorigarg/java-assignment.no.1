afc946d87c87708123d93d92ca1fa38baeba6e9c
kishorigarg <143869466+kishorigarg@users.noreply.github.com>
2023-11-08 12:14:01 +0530

    Add files via upload

---
+++ I&S.java
@@ -0,0 +1,19 @@
+public class ex{
+    public void printArray(String[] s){
+        for(int i=0;i<s.length;i++){
+            System.out.println(s[i]);
+        }
+    }
+    public void printArray(Integer[]s){
+        for(int i=0;i<s.length;i++){
+            System.out.println(s[i]);
+        }
+    }
+        public static void main(String args[]){
+            ex e = new ex();
+            String []s1 = {" Rinki","john"};
+            e.printArray(s1);
+            Integer[]i={1,2,3,4};
+            e.printArray(i);
+            }
+}
