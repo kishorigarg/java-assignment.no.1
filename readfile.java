8831bf789b8c5a9f7b13d31a7b0bb383275ea0ef
kishorigarg <143869466+kishorigarg@users.noreply.github.com>
2023-12-13 12:22:09 +0530

    Add files via upload

---
+++ readfile.java
@@ -0,0 +1,17 @@
+import java.io.FileReader;
+class Main{
+    public static void main(String[]args){
+        char[]array=new char[100];
+     
+        try{
+            FileReader input=new FileReader("a.txt");
+            input.read(array);
+        System.out.println("Data in the file :");
+        System.out.println(array);
+        input.close();
+        }
+    catch(Exception e){
+        e.getStackTrace();
+    }
+    }
+}
